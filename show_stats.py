c0240cca151e45da421675aecaf62a2ea72eb374
Diego Stocco <diego.stocco@cern.ch>
2023-12-04 17:12:20 +0100

    Make linter happy

--- show_stats.py
+++ show_stats.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
+
+"""Show statistics per group"""
+
 import sys
 import argparse
 import hal
 
 
-def _getData(group, docTypes, ymin):
+def _get_data(group, doc_types, ymin):
     entries = {}
-    for dt in docTypes:
-        entries[dt] = hal.getParsed("collCode_s:{} docType_s:{}".format(
-            group, dt), "halId_s,producedDateY_i", ymin)
+    for dt in doc_types:
+        entries[dt] = hal.get_parsed(
+            f"collCode_s:{group} docType_s:{dt}",
+            "halId_s,producedDateY_i",
+            ymin,
+        )
     return entries
 
 
@@ -20,38 +26,37 @@ def _count(entries):
             print("Caveat: no year found in")
             print(entry)
             year = "0"
-        if not year in pubs:
+        if year not in pubs:
             pubs[year] = 0
         pubs[year] += 1
     return pubs
 
 
-def _showStats(entries, min):
+def _show_stats(entries, ymin):
     pubs = _count(entries)
     for key in sorted(pubs.keys(), reverse=True):
-        if (int(key) < min):
+        if int(key) < ymin:
             continue
-        print("{}: {}".format(key, pubs[key]))
+        print(f"{key}: {pubs[key]}")
     return 0
 
 
-def show_stats(group, docTypes, ymin):
-    data = _getData(group, docTypes, ymin)
+def show_stats(group, doc_types, ymin):
+    """Show statistics per group"""
+    data = _get_data(group, doc_types, ymin)
     for key, val in data.items():
         print("Publication type: " + key)
-        _showStats(val, ymin)
+        _show_stats(val, ymin)
     return 0
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Utility for bibliography")
-    parser.add_argument("--group", help="Subatech group",
-                        default="SUBATECH-PLASMA")
-    parser.add_argument("--ymin", help="Minimum year",
-                        type=int, default=2015)
+    parser.add_argument("--group", help="Subatech group", default="SUBATECH-PLASMA")
+    parser.add_argument("--ymin", help="Minimum year", type=int, default=2015)
 
     args = parser.parse_args()
-    docTypes = ["ART", "COMM", "THESE", "POSTER"]
-    rc = show_stats(args.group, docTypes, args.ymin)
+    g_doc_types = ["ART", "COMM", "THESE", "POSTER"]
+    RET_CODE = show_stats(args.group, g_doc_types, args.ymin)
 
-    sys.exit(rc)
+    sys.exit(RET_CODE)
